57d8a30c43f1c968069152c9f226629c07a9678f
Isaac Mbuotidem <mbuotidemisaac@gmail.com>
2021-04-25 00:43:18 -0500

    FN

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.95.2',
+  cdkVersion: '1.99.0',
   defaultReleaseBranch: 'main',
   jsiiFqn: "projen.AwsCdkTypeScriptApp",
   name: 'guichet',
