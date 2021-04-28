ed523a42158ebe64aa4a4358e6f203f28f2950ff
Isaac Mbuotidem <mbuotidemisaac@gmail.com>
2021-04-28 18:15:02 -0500

    Demo

--- lambda/transcriber.js
+++ lambda/transcriber.js
@@ -3,6 +3,6 @@ exports.handler = async function (event) {
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/plain" },
-        body: `Hello! You've hit Guichet at ${event.path}\n`
+        body: `Hello! You're watching the demo, Dr. V at ${event.path}\n`
     };
 };
