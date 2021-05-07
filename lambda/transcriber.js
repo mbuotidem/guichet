5104877f5a1205d6a04a04d3a3c70e70e1fe41b7
Mbuotidem Isaac <mbuotidemisaac@gmail.com>
2021-05-07 01:54:54 -0500

    Troubleshooting dynamodb

--- lambda/transcriber.js
+++ lambda/transcriber.js
@@ -103,7 +103,8 @@ async function getFinishedJob(jobDetails) {
         }
     };
     // Call DynamoDB to add the item to the table
-    ddb.putItem(params, function (err, data) {
+
+    db.putItem(params, function (err, data) {
         if (err) {
             console.log("Error", err);
         } else {
